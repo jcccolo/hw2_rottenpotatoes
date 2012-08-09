6a04900309937fbb34101b5cbd7fed434c05bb82
jcccolo <jon_christopher_connelly@yahoo.com>
2012-08-09 17:39:20 -0600

    changes for homework 2, problem 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort_by = params[:sort_by]
+    @movies = Movie.all({:order => @sort_by})
   end
 
   def new
